7ad1df687ab57d29d7325283fe547db5c4f8b496
zhangminglong <2621968265@qq.com>
2021-01-27 17:09:22 +0800

    Helloword

---
+++ TestStudy/TestStudy/Helloword.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Helloword!\n" << endl;
+
+	return 0;
+}
---
+++ Testmain.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "helloword!\n" << endl;
+
+	return 0;
+
+}
