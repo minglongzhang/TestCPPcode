fce96a42d703cd30f47ba9ed235edc079d93a75d
zhangminglong <2621968265@qq.com>
2021-01-27 17:59:29 +0800

    作业一

--- TestStudy/TestStudy/Helloword.cpp
+++ TestStudy/TestStudy/Helloword.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
+int fun(int x, int y)
+{
+	return x + y;
+}
+
+double fun(double x, double y)
+{
+	return x + y;
+}
+
+
 
 int main()
 {
-	cout << "Helloword!\n" << endl;
+
+	int a = 10, b = 20;
+	double c = 15.5, d = 25.6;
+	float e = 20.2, f = 30.4;
+	cout << fun(a, b) << endl;
+	cout << fun(c, d) << endl;
+	cout << fun(e, f) << endl;
+	//cout << "Helloword!\n" << endl;
 
 	return 0;
 }
